48fe89979b1e333ee5767699e50b87c808b77ef5
Mikat <jsmikat@outlook.com>
2024-05-01 16:58:06 +0600

    Setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="font-poppins bg-primary">Go Daddy</Button>
+    </>
+  );
+}
+
+export default App;
